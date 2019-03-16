f4a6e0298e74872b3880ce2e43fded4baefc8745
kaloyan2021 <kaloyan.elsys2021@gmail.com>
2019-03-16 23:15:41 +0200

    optimised the main.py

--- Software/main.py
+++ Software/main.py
@@ -1,62 +1,59 @@
-from machine import I2C, Pin, Timer
+from machine import I2C, Pin, Timer, rng
 from network import LoRa, WLAN
 import time
 import pycom
 import socket
-import machine
+# import machine
 
-MYID = "1:4"
 wlan = WLAN()
-wlan.init(mode=WLAN.AP, ssid='TEST '+MYID, auth=(WLAN.WPA2,'12345678'))
+wlan.init(mode=WLAN.AP, ssid='TEST ' + MYID, auth=(WLAN.WPA2, '12345678'))
+
 s_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s_UDP.setblocking(False)
-IP_receive = "192.168.4.1"
-IP_send = "192.128.4.2"
-PORT = 5005
 
-pycom.heartbeat(False)
-lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868) # Do we need this???
 s_LORA = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s_LORA.setblocking(False)
+
+global MYID = "1:1"
+global IP_receive = "192.168.4.1"
+global IP_send = "192.128.4.2"
+global PORT = 5005
+
+s_UDP.bind((IP_receive, PORT)) # A problem may occur - if so try writing manually the IP_receive and PORT
+
+def RGB_LED(color, time):
+    pycom.rgbled(color) # set led to <color>
+    time.sleep_ms(time) # for <time> time
+    pycom.rgbled(0) # reset led to none
+
+pycom.heartbeat(False)
 ack_time = Timer.Chrono()
-s_UDP.bind(("192.168.4.1", 5005))
 ack_time.start()
-neighbor_table = ["NT"]
+neighbour_table = ["NT"]
 dead_time = 3
-pycom.rgbled(0xFF00FF)
-time.sleep_ms(1000)
-pycom.rgbled(0)
-
-
+RGB_LED(0xFF00FF, 200)
 
 def SEND_MYID(MYID):
-    NAME = "N," + MYID
-    NAME = bytes(NAME, 'utf-8')
+    NAME = bytes("N,{}".format(MYID), "utf-8") # if there is a problem - split the actions
     s_LORA.send(NAME)
+
 def SEND_ACK(sender):
-    ACK = "A," + sender +","+ MYID
-    ACK= bytes(ACK, 'utf-8')
+    ACK = bytes("A,{},{}".format(sender, MYID)) # if there is a problem - split the actions
     s_LORA.send(ACK)
+
 def SEND_MESSAGE(sender, payload):
-    Message = "M,{},{},{},".format(MYID, sender, payload)
-    Message = Message.encode("utf-8")
-    print(Message)
-    s_LORA.send(Message)
-def ADDING(sender,neighbor_table):
-    print(neighbor_table)
-    l = len(neighbor_table)
-    for i in range(0,l):
-        if sender == neighbor_table[i] and sender != MYID:
-            checked = 0
-        else:
-            checked = 1
-    if checked == 1:
-        neighbor_table.append(sender)
-    else:
-        pass
-    return neighbor_table
+    s_LORA.send("M,{},{},{},".format(MYID, sender, payload).encode("utf-8")) # if there is a problem - split the argument
+
+def ADDING(sender, neighbour_table): # Checks the neighbour_table and if sender (id) is not in it - adds it
+    for i in range(len(neighbour_table)):
+        if sender == neighbour_table[i] and sender != MYID:
+            return neighbour_table # sender is in neighbour_table -> no need to add again
+    neighbour_table.append(sender)
+    return neighbour_table
+
 def TIME(min_wait):
-    min_wait = min_wait + (machine.rng()/10000000)
+    min_wait = min_wait + (rng()/10000000) # if problem occurs try to import machine and use ... + (machine.rng/10000000)
     time.sleep(min_wait)
 
 while(True):
@@ -65,46 +62,29 @@ while(True):
         SEND_MYID(MYID)
         ack_time.reset()
 
-    received_information_LORA = s_LORA.recv(128)
-    received_information_UDP = s_UDP.recv(128)
+    recv_info_LORA = s_LORA.recv(128)
+    recv_info_UDP = s_UDP.recv(128)
 
+    if (recv_info_LORA):
+        recv_info_LORA = str(recv_info_LORA.decode("utf-8")).split(',')
+        if recv_info_LORA[0] == "N":
+            neighbour_table = ADDING(recv_info_LORA[1], neighbour_table)
+            SEND_ACK(recv_info_LORA[1])
+            RGB_LED(0x0000FF, 20)
 
-    if (received_information_LORA):
-        received_information_LORA = received_information_LORA.decode("utf-8")
-        received_information_LORA = str(received_information_LORA).split(',')
+        if recv_info_LORA[0] == "A" and recv_info_LORA[2] == MYID:
+            neighbour_table = ADDING(recv_info_LORA[1], neighbour_table)
+            RGB_LED(0x00FF00, 20)
 
-        if received_information_LORA[0] == "N":
-            sender = received_information_LORA[1]
-            neighbor_table = ADDING(sender,neighbor_table)
-            print(neighbor_table)
-            SEND_ACK(sender)
-            pycom.rgbled(0x0000FF)
-            time.sleep_ms(10)
-            pycom.rgbled(0)
+        if recv_info_LORA[0] == "M" and recv_info_LORA[2] == MYID:
+            s_UDP.sendto(recv_info_LORA[3], (IP_send,PORT))
+            RGB_LED(0xFFFF00, 20)
 
-        if received_information_LORA[0] == "A" and received_information_LORA[2] == MYID:
-            sender = received_information_LORA[1]
-            neighbor_table = ADDING(sender,neighbor_table)
-            pycom.rgbled(0x00FF00)
-            time.sleep_ms(10)
-            pycom.rgbled(0)
-
-        if received_information_LORA[0] == "M" and received_information_LORA[2] == MYID:
-            pycom.rgbled(0xFFFF00)
-            time.sleep_ms(10)
-            pycom.rgbled(0)
-            end_message = received_information_LORA[3]
-            print(end_message)
-            s_UDP.sendto(end_message,("192.168.4.2",5005))
-
-    if(received_information_UDP):
-        payload = received_information_UDP
-        print(payload)
+    if(recv_info_UDP):
         try:
-            sender = neighbor_table[1]
-            SEND_MESSAGE(sender,payload.decode("utf-8"))
-            pycom.rgbled(0xFFFFFF)
-            time.sleep_ms(10)
-            pycom.rgbled(0)
+            SEND_MESSAGE(neighbour_table[1], recv_info_UDP.decode("utf-8"))
+            RGB_LED(0xFFFFFF, 20)
+
         except IndexError as err:
+            print(err)
             continue
